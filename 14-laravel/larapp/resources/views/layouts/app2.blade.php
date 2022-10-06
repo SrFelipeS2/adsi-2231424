54e2f36836b7aeab7779308ece979c73965cfb87
Andres Felipe Valencia Lopez <andresvalencialopez244@gmail.com>
2022-10-05 19:28:52 -0500

    agregamos conocimientos basicos en Laravel

--- 14-laravel/larapp/resources/views/layouts/app2.blade.php
+++ 14-laravel/larapp/resources/views/layouts/app2.blade.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
- 
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
- 
+
     <title>{{ config('app.name', 'Laravel') }}</title>
- 
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    
+
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
@@ -32,7 +32,7 @@
                         @endif
                     @else
                         <span>{{ Auth::user()->name }}</span>
- 
+
                         <a href="{{ route('logout') }}"
                            class="no-underline hover:underline"
                            onclick="event.preventDefault();
@@ -44,7 +44,7 @@
                 </nav>
             </div>
         </header>
- 
+
         @yield('content')
     </div>
 </body>
